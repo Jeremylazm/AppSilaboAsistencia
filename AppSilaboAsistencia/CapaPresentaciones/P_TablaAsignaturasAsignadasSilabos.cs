d98d74b8e1a34fb8d245bb80fa41d40011727519
denisomarcuyottito <182906@unsaac.edu.pe>
2021-11-22 16:51:06 -0500

    Update P_TablaAsignaturasAsignadasSilabos.cs
    Mostar asignaturas que dicta el docente

--- AppSilaboAsistencia/CapaPresentaciones/P_TablaAsignaturasAsignadasSilabos.cs
+++ AppSilaboAsistencia/CapaPresentaciones/P_TablaAsignaturasAsignadasSilabos.cs
@@ -11,6 +11,20 @@ namespace CapaPresentaciones
         {
             InitializeComponent();
             Bunifu.Utils.DatagridView.BindDatagridViewScrollBar(dgvDatos, sbDatos);
+            MostrarAsignaturas();
+        }
+
+        private void AccionesTabla()
+        {
+            dgvDatos.Columns[0].DisplayIndex = 6;
+            dgvDatos.Columns[1].DisplayIndex = 6;
+            dgvDatos.Columns[2].DisplayIndex = 6;
+        }
+
+        private void MostrarAsignaturas()
+        {
+            dgvDatos.DataSource = N_Catalogo.BuscarAsignaturasDocente("2021-II", "IF", "10134");
+            AccionesTabla();
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
