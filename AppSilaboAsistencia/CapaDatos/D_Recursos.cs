b9514d9e115b0e8ba9dfddb02d730b5d95b666c4
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2021-11-22 19:56:02 -0500

    Update Capas

---
+++ AppSilaboAsistencia/CapaDatos/D_Recursos.cs
@@ -0,0 +1,68 @@
+﻿using CapaEntidades;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class D_Recursos
+    {
+        readonly SqlConnection Conectar = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+
+        // Método para descargar la plantilla del silabo.
+        public DataTable DescargarPlantillaSilabo()
+        {
+            DataTable Resultado = new DataTable();
+            SqlCommand Comando = new SqlCommand("spuDescargarPlantillaSilabo", Conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            SqlDataAdapter Data = new SqlDataAdapter(Comando);
+            Data.Fill(Resultado);
+            return Resultado;
+        }
+
+        // Método para descargar la plantilla del plan de sesiones.
+        public DataTable DescargarPlantillaPlanSesiones()
+        {
+            DataTable Resultado = new DataTable();
+            SqlCommand Comando = new SqlCommand("spuDescargarPlantillaPlanSesiones", Conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            SqlDataAdapter Data = new SqlDataAdapter(Comando);
+            Data.Fill(Resultado);
+            return Resultado;
+        }
+
+        // Método para actualizar la plantilla del silabo
+        public void ActualizarPlantillaSilabo(E_Recursos Recursos)
+        {
+            SqlCommand Comando = new SqlCommand("spuActualizarPlantillaSilabo", Conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            Conectar.Open();
+            Comando.Parameters.AddWithValue("@PlantillaSilabo", Recursos.PlantillaSilabo);
+            Comando.ExecuteNonQuery();
+            Conectar.Close();
+        }
+
+        // Método para actualizar la plantilla del plan de sesiones
+        public void ActualizarPlantillaPlanSesiones(E_Recursos Recursos)
+        {
+            SqlCommand Comando = new SqlCommand("spuActualizarPlantillaPlanSesiones", Conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            Conectar.Open();
+            Comando.Parameters.AddWithValue("@PlantillaPlanSesiones", Recursos.PlantillaPlanSesiones);
+            Comando.ExecuteNonQuery();
+            Conectar.Close();
+        }
+    }
+}
---
+++ AppSilaboAsistencia/CapaNegocios/N_Recursos.cs
@@ -0,0 +1,28 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System.Data;
+
+namespace CapaNegocios
+{
+    public class N_Recursos
+    {
+        readonly D_Recursos ObjRecursos = new D_Recursos();
+
+        public static DataTable DescargarPlantillaSilabo()
+        {
+            return new D_Recursos().DescargarPlantillaSilabo();
+        }
+        public static DataTable DescargarPlantillaPlanSesiones()
+        {
+            return new D_Recursos().DescargarPlantillaPlanSesiones();
+        }
+        public void ActualizarPlantillaSilabo(E_Recursos Recursos)
+        {
+            ObjRecursos.ActualizarPlantillaSilabo(Recursos);
+        }
+        public void ActualizarPlantillaPlanSesiones(E_Recursos Recursos)
+        {
+            ObjRecursos.ActualizarPlantillaPlanSesiones(Recursos);
+        }
+    }
+}
