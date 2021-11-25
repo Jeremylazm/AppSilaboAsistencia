f7916dc7528692821ee015238cec03f53f82de75
JamesKevinStar <182909@unsaac.edu.pe>
2021-11-24 22:06:21 -0500

    Agregar Interfaz Catálogo

---
+++ AppSilaboAsistencia/CapaPresentaciones/P_Catálogo_Actualizar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentaciones
+{
+    public partial class P_Catálogo_Actualizar : Form
+    {
+        public P_Catálogo_Actualizar()
+        {
+            InitializeComponent();
+        }
+    }
+}
