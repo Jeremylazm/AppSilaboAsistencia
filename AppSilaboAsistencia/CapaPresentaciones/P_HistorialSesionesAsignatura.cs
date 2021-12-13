e3b8b9bcee80f6424c38f60c30118fef7f337e9d
Jeremylazm <72510370+Jeremylazm@users.noreply.github.com>
2021-12-13 13:10:37 -0500

    Interfaz de Historial de Sesiones

---
+++ AppSilaboAsistencia/CapaPresentaciones/P_HistorialSesionesAsignatura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentaciones
+{
+    public partial class P_HistorialSesionesAsignatura : Form
+    {
+        public string CodAsignatura;
+        public string CodDocente;
+
+        public P_HistorialSesionesAsignatura(string pCodAsignatura, string pCodDocente)
+        {
+            CodAsignatura = pCodAsignatura;
+            CodDocente = pCodDocente;
+            InitializeComponent();
+
+            Bunifu.Utils.DatagridView.BindDatagridViewScrollBar(dgvDatos, sbDatos);
+            lblTitulo.Text += CodAsignatura;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
