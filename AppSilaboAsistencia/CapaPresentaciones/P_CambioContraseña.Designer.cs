37a3342e23a0b91df8fe92aaa47eac0af303a78e
Jeremylazm <72510370+Jeremylazm@users.noreply.github.com>
2021-12-13 23:59:51 -0500

    Interfaz Cambio de Contraseña

---
+++ AppSilaboAsistencia/CapaPresentaciones/P_CambioContraseña.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace CapaPresentaciones
+{
+    partial class P_CambioContraseña
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(P_CambioContraseña));
+            this.lblTitulo = new Bunifu.UI.WinForms.BunifuLabel();
+            this.btnCerrar = new Bunifu.UI.WinForms.BunifuImageButton();
+            this.pnContenedor = new Bunifu.UI.WinForms.BunifuPanel();
+            this.pbPaso1 = new Bunifu.UI.WinForms.BunifuPictureBox();
+            this.Bordeado = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.lblPaso1 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.lblPaso2 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.pbPaso2 = new Bunifu.UI.WinForms.BunifuPictureBox();
+            this.lblPaso3 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.pbPaso3 = new Bunifu.UI.WinForms.BunifuPictureBox();
+            this.bunifuSeparator1 = new Bunifu.UI.WinForms.BunifuSeparator();
+            this.bunifuSeparator2 = new Bunifu.UI.WinForms.BunifuSeparator();
+            this.pnPasos = new Bunifu.UI.WinForms.BunifuPanel();
+            this.pnContenedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPaso1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPaso2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPaso3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AllowParentOverrides = false;
+            this.lblTitulo.AutoEllipsis = false;
+            this.lblTitulo.AutoSize = false;
+            this.lblTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(13)))), ((int)(((byte)(15)))));
+            this.lblTitulo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblTitulo.CursorType = System.Windows.Forms.Cursors.Default;
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Font = new System.Drawing.Font("Montserrat Alternates", 12F);
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(158)))), ((int)(((byte)(31)))));
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblTitulo.Size = new System.Drawing.Size(791, 46);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Cambio de Contraseña";
+            this.lblTitulo.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitulo.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.ActiveImage = null;
+            this.btnCerrar.AllowAnimations = true;
+            this.btnCerrar.AllowBuffering = false;
+            this.btnCerrar.AllowToggling = false;
+            this.btnCerrar.AllowZooming = true;
+            this.btnCerrar.AllowZoomingOnFocus = false;
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(13)))), ((int)(((byte)(15)))));
+            this.btnCerrar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnCerrar.ErrorImage = null;
+            this.btnCerrar.FadeWhenInactive = false;
+            this.btnCerrar.Flip = Bunifu.UI.WinForms.BunifuImageButton.FlipOrientation.Normal;
+            this.btnCerrar.Image = global::CapaPresentaciones.Properties.Resources.Cerrar;
+            this.btnCerrar.ImageActive = null;
+            this.btnCerrar.ImageLocation = null;
+            this.btnCerrar.ImageMargin = 10;
+            this.btnCerrar.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnCerrar.ImageZoomSize = new System.Drawing.Size(30, 30);
+            this.btnCerrar.InitialImage = null;
+            this.btnCerrar.Location = new System.Drawing.Point(753, 7);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Rotation = 0;
+            this.btnCerrar.ShowActiveImage = true;
+            this.btnCerrar.ShowCursorChanges = true;
+            this.btnCerrar.ShowImageBorders = true;
+            this.btnCerrar.ShowSizeMarkers = false;
+            this.btnCerrar.Size = new System.Drawing.Size(30, 30);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.ToolTipText = "";
+            this.btnCerrar.WaitOnLoad = false;
+            this.btnCerrar.Zoom = 10;
+            this.btnCerrar.ZoomSpeed = 10;
+            // 
+            // pnContenedor
+            // 
+            this.pnContenedor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnContenedor.BackgroundColor = System.Drawing.Color.White;
+            this.pnContenedor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnContenedor.BackgroundImage")));
+            this.pnContenedor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnContenedor.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(13)))), ((int)(((byte)(15)))));
+            this.pnContenedor.BorderRadius = 20;
+            this.pnContenedor.BorderThickness = 1;
+            this.pnContenedor.Controls.Add(this.pnPasos);
+            this.pnContenedor.Controls.Add(this.lblPaso3);
+            this.pnContenedor.Controls.Add(this.pbPaso1);
+            this.pnContenedor.Controls.Add(this.pbPaso2);
+            this.pnContenedor.Controls.Add(this.pbPaso3);
+            this.pnContenedor.Controls.Add(this.lblPaso2);
+            this.pnContenedor.Controls.Add(this.lblPaso1);
+            this.pnContenedor.Controls.Add(this.bunifuSeparator2);
+            this.pnContenedor.Controls.Add(this.bunifuSeparator1);
+            this.pnContenedor.Controls.Add(this.btnCerrar);
+            this.pnContenedor.Controls.Add(this.lblTitulo);
+            this.pnContenedor.Location = new System.Drawing.Point(6, 5);
+            this.pnContenedor.Name = "pnContenedor";
+            this.pnContenedor.ShowBorders = true;
+            this.pnContenedor.Size = new System.Drawing.Size(791, 453);
+            this.pnContenedor.TabIndex = 17;
+            // 
+            // pbPaso1
+            // 
+            this.pbPaso1.AllowFocused = false;
+            this.pbPaso1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbPaso1.AutoSizeHeight = true;
+            this.pbPaso1.BorderRadius = 25;
+            this.pbPaso1.Image = global::CapaPresentaciones.Properties.Resources.Circulo_Unchecked;
+            this.pbPaso1.IsCircle = true;
+            this.pbPaso1.Location = new System.Drawing.Point(15, 58);
+            this.pbPaso1.Name = "pbPaso1";
+            this.pbPaso1.Size = new System.Drawing.Size(50, 50);
+            this.pbPaso1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPaso1.TabIndex = 12;
+            this.pbPaso1.TabStop = false;
+            this.pbPaso1.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Circle;
+            // 
+            // Bordeado
+            // 
+            this.Bordeado.ElipseRadius = 15;
+            this.Bordeado.TargetControl = this.pnContenedor;
+            // 
+            // lblPaso1
+            // 
+            this.lblPaso1.AllowParentOverrides = false;
+            this.lblPaso1.AutoEllipsis = false;
+            this.lblPaso1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblPaso1.CursorType = System.Windows.Forms.Cursors.Default;
+            this.lblPaso1.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaso1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(13)))), ((int)(((byte)(15)))));
+            this.lblPaso1.Location = new System.Drawing.Point(71, 70);
+            this.lblPaso1.Name = "lblPaso1";
+            this.lblPaso1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPaso1.Size = new System.Drawing.Size(161, 23);
+            this.lblPaso1.TabIndex = 13;
+            this.lblPaso1.Text = "Correo Institucional";
+            this.lblPaso1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblPaso1.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // lblPaso2
+            // 
+            this.lblPaso2.AllowParentOverrides = false;
+            this.lblPaso2.AutoEllipsis = false;
+            this.lblPaso2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblPaso2.CursorType = System.Windows.Forms.Cursors.Default;
+            this.lblPaso2.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaso2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(158)))), ((int)(((byte)(31)))));
+            this.lblPaso2.Location = new System.Drawing.Point(339, 70);
+            this.lblPaso2.Name = "lblPaso2";
+            this.lblPaso2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPaso2.Size = new System.Drawing.Size(181, 23);
+            this.lblPaso2.TabIndex = 15;
+            this.lblPaso2.Text = "Código de Verificación";
+            this.lblPaso2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblPaso2.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // pbPaso2
+            // 
+            this.pbPaso2.AllowFocused = false;
+            this.pbPaso2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbPaso2.AutoSizeHeight = true;
+            this.pbPaso2.BorderRadius = 25;
+            this.pbPaso2.Image = global::CapaPresentaciones.Properties.Resources.Circulo;
+            this.pbPaso2.IsCircle = true;
+            this.pbPaso2.Location = new System.Drawing.Point(283, 58);
+            this.pbPaso2.Name = "pbPaso2";
+            this.pbPaso2.Size = new System.Drawing.Size(50, 50);
+            this.pbPaso2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPaso2.TabIndex = 14;
+            this.pbPaso2.TabStop = false;
+            this.pbPaso2.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Circle;
+            // 
+            // lblPaso3
+            // 
+            this.lblPaso3.AllowParentOverrides = false;
+            this.lblPaso3.AutoEllipsis = false;
+            this.lblPaso3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblPaso3.CursorType = System.Windows.Forms.Cursors.Default;
+            this.lblPaso3.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaso3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(158)))), ((int)(((byte)(31)))));
+            this.lblPaso3.Location = new System.Drawing.Point(621, 70);
+            this.lblPaso3.Name = "lblPaso3";
+            this.lblPaso3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPaso3.Size = new System.Drawing.Size(154, 23);
+            this.lblPaso3.TabIndex = 17;
+            this.lblPaso3.Text = "Nueva Contraseña";
+            this.lblPaso3.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblPaso3.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // pbPaso3
+            // 
+            this.pbPaso3.AllowFocused = false;
+            this.pbPaso3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbPaso3.AutoSizeHeight = true;
+            this.pbPaso3.BorderRadius = 25;
+            this.pbPaso3.Image = global::CapaPresentaciones.Properties.Resources.Circulo;
+            this.pbPaso3.IsCircle = true;
+            this.pbPaso3.Location = new System.Drawing.Point(565, 58);
+            this.pbPaso3.Name = "pbPaso3";
+            this.pbPaso3.Size = new System.Drawing.Size(50, 50);
+            this.pbPaso3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPaso3.TabIndex = 16;
+            this.pbPaso3.TabStop = false;
+            this.pbPaso3.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Circle;
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuSeparator1.BackgroundImage")));
+            this.bunifuSeparator1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuSeparator1.DashCap = Bunifu.UI.WinForms.BunifuSeparator.CapStyles.Flat;
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(158)))), ((int)(((byte)(31)))));
+            this.bunifuSeparator1.LineStyle = Bunifu.UI.WinForms.BunifuSeparator.LineStyles.Solid;
+            this.bunifuSeparator1.LineThickness = 2;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(61, 79);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Orientation = Bunifu.UI.WinForms.BunifuSeparator.LineOrientation.Horizontal;
+            this.bunifuSeparator1.Size = new System.Drawing.Size(225, 10);
+            this.bunifuSeparator1.TabIndex = 18;
+            // 
+            // bunifuSeparator2
+            // 
+            this.bunifuSeparator2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuSeparator2.BackgroundImage")));
+            this.bunifuSeparator2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuSeparator2.DashCap = Bunifu.UI.WinForms.BunifuSeparator.CapStyles.Flat;
+            this.bunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(158)))), ((int)(((byte)(31)))));
+            this.bunifuSeparator2.LineStyle = Bunifu.UI.WinForms.BunifuSeparator.LineStyles.Solid;
+            this.bunifuSeparator2.LineThickness = 2;
+            this.bunifuSeparator2.Location = new System.Drawing.Point(332, 79);
+            this.bunifuSeparator2.Name = "bunifuSeparator2";
+            this.bunifuSeparator2.Orientation = Bunifu.UI.WinForms.BunifuSeparator.LineOrientation.Horizontal;
+            this.bunifuSeparator2.Size = new System.Drawing.Size(299, 10);
+            this.bunifuSeparator2.TabIndex = 19;
+            // 
+            // pnPasos
+            // 
+            this.pnPasos.BackgroundColor = System.Drawing.Color.Transparent;
+            this.pnPasos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnPasos.BackgroundImage")));
+            this.pnPasos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnPasos.BorderColor = System.Drawing.Color.Transparent;
+            this.pnPasos.BorderRadius = 3;
+            this.pnPasos.BorderThickness = 1;
+            this.pnPasos.Location = new System.Drawing.Point(17, 121);
+            this.pnPasos.Name = "pnPasos";
+            this.pnPasos.ShowBorders = true;
+            this.pnPasos.Size = new System.Drawing.Size(758, 314);
+            this.pnPasos.TabIndex = 20;
+            // 
+            // P_CambioContraseña
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(803, 463);
+            this.Controls.Add(this.pnContenedor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "P_CambioContraseña";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tabla de Asignaturas Asignadas - Estudiantes";
+            this.pnContenedor.ResumeLayout(false);
+            this.pnContenedor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPaso1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPaso2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPaso3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Bunifu.UI.WinForms.BunifuLabel lblTitulo;
+        private Bunifu.UI.WinForms.BunifuImageButton btnCerrar;
+        private Bunifu.UI.WinForms.BunifuPanel pnContenedor;
+        private Bunifu.Framework.UI.BunifuElipse Bordeado;
+        private Bunifu.UI.WinForms.BunifuPictureBox pbPaso1;
+        private Bunifu.UI.WinForms.BunifuLabel lblPaso1;
+        private Bunifu.UI.WinForms.BunifuPanel pnPasos;
+        private Bunifu.UI.WinForms.BunifuLabel lblPaso3;
+        private Bunifu.UI.WinForms.BunifuPictureBox pbPaso2;
+        private Bunifu.UI.WinForms.BunifuPictureBox pbPaso3;
+        private Bunifu.UI.WinForms.BunifuLabel lblPaso2;
+        private Bunifu.UI.WinForms.BunifuSeparator bunifuSeparator2;
+        private Bunifu.UI.WinForms.BunifuSeparator bunifuSeparator1;
+    }
+}
