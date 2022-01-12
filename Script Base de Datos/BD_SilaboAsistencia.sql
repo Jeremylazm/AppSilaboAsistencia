408c0f55b42c5dd4b5a68cb45c69fd879cb3a031
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2022-01-12 16:13:31 -0500

    eliminar consultas

--- Script Base de Datos/BD_SilaboAsistencia.sql
+++ Script Base de Datos/BD_SilaboAsistencia.sql
@@ -1407,14 +1407,12 @@ CREATE PROCEDURE spuMostrarEstudiantesMatriculados @CodSemestre VARCHAR(7),
 AS
 BEGIN
 	-- Mostrar la tabla de TMatricula
-	SELECT CodEstudiante, APaterno, AMaterno, Nombre
+	SELECT DISTINCT CodEstudiante, APaterno, AMaterno, Nombre
 		FROM TMatricula
 	    WHERE CodSemestre = @CodSemestre AND CodEscuelaP = @CodEscuelaP
 		ORDER BY APaterno ASC
 END;
 GO
-
-exec spuMostrarEstudiantesMatriculados '2021-II','IN'
 
 -- Procedimiento para buscar las asignaturas a los que esta matriculado un estudiante de una escuela profesional.
 CREATE PROCEDURE spuBuscarAsignaturasEstudiante @CodSemestre VARCHAR(7),
@@ -1480,10 +1478,6 @@ BEGIN
 		VALUES (@CodSemestre, @CodEscuelaP, @CodAsignatura, @CodEstudiante, @APaterno, @AMaterno, @Nombre)
 END;
 GO
-
-EXEC spuInsertarMatricula '2021-II','IN','IF650AIN','204797','HANCCO','CHAMPI','FRAN ANTHONY'
-EXEC spuInsertarMatricula '2021-II','IN','IF467AIN','204797','HANCCO','CHAMPI','FRAN ANTHONY'
-
 
 -- Procedimiento para actualizar la matricula de un estudiante.
 CREATE PROCEDURE spuActualizarMatricula @CodSemestre VARCHAR(7),
