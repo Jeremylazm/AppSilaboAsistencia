96b7499ff520c7f445860ad49a48e77b96374c57
JamesKevinStar <182909@unsaac.edu.pe>
2022-01-13 21:47:51 -0500

    Agregar las funciones de los Reportes 5 y 9 para el Jefe de Departamento

--- AppSilaboAsistencia/CapaPresentaciones/P_ReporteJefe.cs
+++ AppSilaboAsistencia/CapaPresentaciones/P_ReporteJefe.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocios;
+using CapaEntidades;
+using ControlesPerzonalizados;
 
 namespace CapaPresentaciones
 {
     public partial class P_ReporteJefe : Form
     {
+        readonly N_Catalogo ObjCatalogo;
+        private readonly string CodSemestre;
+        readonly string CodDocente = E_InicioSesion.Usuario;
+        private readonly string CodDepartamentoA = E_InicioSesion.CodDepartamentoA;
+        C_Reporte Reportes = new C_Reporte();
+        string nombreDocente;
         public P_ReporteJefe()
         {
             InitializeComponent();
@@ -20,6 +31,131 @@ namespace CapaPresentaciones
         private void btnSeleccionar_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void fnReporte5()
+        {
+            // Tipo de reporte: Avance Asignatura
+            // Criterio de selección: Por Docente
+            string Titulo = "REPORTE DE AVANCE" + Environment.NewLine + "DEL SEMESTRE " + CodSemestre;
+            string[] Titulos = { "Semestre", "Cod. Docente", "Docente", "Cod. Asignatura", "Asignatura", "Escuela Profesional" };
+            string[] Valores = { CodSemestre, CodDocente, nombreDocente, txtCodigo.Text, txtNombre.Text, txtEscuelaP.Text };
+
+            DataTable resultados = N_AsistenciaDocentePorAsignatura.AvanceAsignatura(CodSemestre, CodDocente, txtCodigo.Text);
+            DataTable plansesion = N_Catalogo.RecuperarPlanDeSesionAsignatura(CodSemestre, txtCodigo.Text, CodDocente);
+
+            int Total = 0;
+
+            if (plansesion.Rows.Count >= 1)
+            {
+                DataRow Fila = plansesion.Rows[0];
+
+                byte[] archivo = Fila["PlanSesiones"] as byte[];
+
+                string path = AppDomain.CurrentDomain.BaseDirectory;
+                string folder = path + "/temp/";
+                string fullFilePath = folder + "temp.xlsx";
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                if (File.Exists(fullFilePath))
+                {
+                    File.Delete(fullFilePath);
+                }
+
+                File.WriteAllBytes(fullFilePath, archivo);
+                XLWorkbook wb = new XLWorkbook(fullFilePath);
+
+                DataTable ResultadosFinales = new DataTable();
+                ResultadosFinales.Columns.Add("Sesión", typeof(int));
+                ResultadosFinales.Columns.Add("NombreTema", typeof(string));
+                ResultadosFinales.Columns.Add("Fecha", typeof(string));
+                ResultadosFinales.Columns.Add("Estado", typeof(string));
+
+                int[] TemasAvanzados = new int[resultados.Rows.Count];
+
+                for (int i = 0; i < resultados.Rows.Count; i++)
+                {
+                    TemasAvanzados[i] = Convert.ToInt32(resultados.Rows[i]["Sesión"].ToString());
+                    ResultadosFinales.Rows.Add(Convert.ToInt32(resultados.Rows[i]["Sesión"].ToString()), resultados.Rows[i]["NombreTema"].ToString(), resultados.Rows[i]["Fecha"].ToString(), "HECHO");
+                }
+
+                int Contador = 9;
+                for (int i = 9; i <= 61; i++)
+                {
+                    if (wb.Worksheet(1).Cell("E" + Convert.ToString(i)).Value.ToString() != "")
+                    {
+                        if (Array.Exists(TemasAvanzados, x => x == Convert.ToInt32(wb.Worksheet(1).Cell("B" + Convert.ToString(i)).Value.ToString())))
+                        {
+
+                        }
+                        else
+                            ResultadosFinales.Rows.Add(Contador - 8, "Tema" + Convert.ToString(Contador - 8), "", "FALTA");
+                        Total = Total + 1;
+                        Contador = Contador + 1;
+                    }
+                }
+
+                int Hechos = resultados.Rows.Count;
+                int Faltan = Total - Hechos;
+                Reportes.fnReporte5(Titulo, Titulos, Valores, ResultadosFinales, txtCodigo.Text, Hechos, Faltan);
+            }
+            else
+                Ayudas.A_Dialogo.DialogoError("No hay Plan de Sesiones");
+        }
+
+        private void fnReporte9()
+        {
+            string Titulo = "REPORTE DE AVANCE DE ASIGNATURA" + Environment.NewLine + "DEL SEMESTRE " + CodSemestre;
+            string[] Titulos = { "Semestre" };
+            string[] Valores = { CodSemestre };
+
+            DataTable resultados = N_AsistenciaDocentePorAsignatura.AvanceAsignaturasDpto(CodSemestre, CodDepartamentoA);
+
+            DataTable ResultadosFinales = new DataTable();
+            ResultadosFinales.Columns.Add("CodAsignatura", typeof(string));
+            ResultadosFinales.Columns.Add("NombreAsignatura", typeof(string));
+            ResultadosFinales.Columns.Add("Docente", typeof(string));
+            ResultadosFinales.Columns.Add("TemasAvanzados", typeof(string));
+            ResultadosFinales.Columns.Add("TemasFaltantes", typeof(string));
+
+            DataTable ResultadosResumen = new DataTable();
+            ResultadosResumen.Columns.Add("CodAsignatura", typeof(string));
+            ResultadosResumen.Columns.Add("NombreAsignatura", typeof(string));
+            ResultadosResumen.Columns.Add("Docente", typeof(string));
+            ResultadosResumen.Columns.Add("TemasAvanzados", typeof(int));
+            ResultadosResumen.Columns.Add("TemasFaltantes", typeof(int));
+
+            int Créditos = 0;
+            int TemasTotales = 0;
+            float PorcentajeAvanzados = 0;
+
+            for (int i = 0; i < resultados.Rows.Count; i++)
+            {
+                DataTable TablaCreditos = N_Asignatura.BuscarAsignatura(CodDepartamentoA, resultados.Rows[i]["CodAsignatura"].ToString().Substring(0, 5));
+                if (TablaCreditos.Rows.Count != 0)
+                {
+                    Créditos = Convert.ToInt32(TablaCreditos.Rows[0]["Creditos"].ToString());
+                }
+                if (Créditos == 4)
+                {
+                    TemasTotales = 51;
+                    PorcentajeAvanzados = 100 * Convert.ToInt32(resultados.Rows[i]["TemasAvanzados"].ToString()) / TemasTotales;
+                    ResultadosFinales.Rows.Add(resultados.Rows[i]["CodAsignatura"].ToString(), resultados.Rows[i]["NombreAsignatura"].ToString(), resultados.Rows[i]["Docente"].ToString(), Convert.ToString(PorcentajeAvanzados) + "%", Convert.ToString(100 - PorcentajeAvanzados) + "%");
+                    ResultadosResumen.Rows.Add(resultados.Rows[i]["CodAsignatura"].ToString(), resultados.Rows[i]["NombreAsignatura"].ToString(), resultados.Rows[i]["Docente"].ToString(), Convert.ToInt32(resultados.Rows[i]["TemasAvanzados"].ToString()), TemasTotales - Convert.ToInt32(resultados.Rows[i]["TemasAvanzados"].ToString()));
+                }
+                else
+                {
+                    TemasTotales = 34;
+                    PorcentajeAvanzados = 100 * Convert.ToInt32(resultados.Rows[i]["TemasAvanzados"].ToString()) / TemasTotales;
+                    ResultadosFinales.Rows.Add(resultados.Rows[i]["CodAsignatura"].ToString(), resultados.Rows[i]["NombreAsignatura"].ToString(), resultados.Rows[i]["Docente"].ToString(), Convert.ToString(PorcentajeAvanzados) + "%", Convert.ToString(100 - PorcentajeAvanzados) + "%");
+                    ResultadosResumen.Rows.Add(resultados.Rows[i]["CodAsignatura"].ToString(), resultados.Rows[i]["NombreAsignatura"].ToString(), resultados.Rows[i]["Docente"].ToString(), Convert.ToInt32(resultados.Rows[i]["TemasAvanzados"].ToString()), TemasTotales - Convert.ToInt32(resultados.Rows[i]["TemasAvanzados"].ToString()));
+                }
+            }
+
+            Reportes.fnReporte9(Titulo, Titulos, Valores, ResultadosFinales, ResultadosResumen);
         }
     }
 }
